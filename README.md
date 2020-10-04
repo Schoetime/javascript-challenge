afe4f3a7c21baaaaeefa62f7793e9f183f2a5d51
Jeremy Schoenberg <jeremy.schoenberg@gmail.com>
2020-10-03 22:04:44 -0700

    adding event handler code to app.js and adding an updated read.me

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javascript-challenge
+
+For this project, I added javascript code that uploaded table data from a data.js file and then created an event handler function that would display filtered table data based on user imput.
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,6 +7,9 @@ let tbody = d3.select("tbody");
 //Select the table from D3
 let table = d3.select("table");
 
+let filterButton = d3.select("#filter-btn"),
+    form = d3.select("#form");
+
 //Fill the table with the UFO sighting data
 data.forEach(UFOsighting => {
     let row = tbody.append("tr");
@@ -15,7 +18,42 @@ data.forEach(UFOsighting => {
     row.append("td").text(UFOsighting.state);
     row.append("td").text(UFOsighting.country);
     row.append("td").text(UFOsighting.shape);
+    row.append("td").text(UFOsighting.durationMinutes);
     row.append("td").text(UFOsighting.comments);
 });
 
+// Complete the event handler function
+const runEnter = () => {
 
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    // Get the value property of the input element
+    let inputEntry = d3.select("#datetime"), 
+        inputValue = inputEntry.property("value");
+
+        console.log(inputEntry);
+        console.log(inputValue);
+
+    let filteredData = tableData.filter(UFOsighting => UFOsighting.datetime === inputValue);
+
+    //Remove all UFO sighting data from the table
+    tbody.html("");
+
+    //Create a new filtered table
+    filteredData.forEach(UFOsighting => {
+        let row = tbody.append("tr");
+        row.append("td").text(UFOsighting.datetime);
+        row.append("td").text(UFOsighting.city);
+        row.append("td").text(UFOsighting.state);
+        row.append("td").text(UFOsighting.country);
+        row.append("td").text(UFOsighting.shape);
+        row.append("td").text(UFOsighting.durationMinutes);
+        row.append("td").text(UFOsighting.comments);
+    });
+}; 
+
+// Create event handlers 
+filterButton.on("click", runEnter);
+form.on("submit",runEnter);
