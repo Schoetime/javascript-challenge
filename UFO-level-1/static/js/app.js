bbe86d40e097771c30a976cb7ed7d77f35eca87d
Jeremy Schoenberg <jeremy.schoenberg@gmail.com>
2020-10-03 21:33:24 -0700

    adding app.js file with appended table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,21 @@
-// from data.js
-var tableData = data;
+// Retrieve the UFO sightings from data.js
+let tableData = data;
+
+//Get a reference to the table body from D3
+let tbody = d3.select("tbody");
+
+//Select the table from D3
+let table = d3.select("table");
+
+//Fill the table with the UFO sighting data
+data.forEach(UFOsighting => {
+    let row = tbody.append("tr");
+    row.append("td").text(UFOsighting.datetime);
+    row.append("td").text(UFOsighting.city);
+    row.append("td").text(UFOsighting.state);
+    row.append("td").text(UFOsighting.country);
+    row.append("td").text(UFOsighting.shape);
+    row.append("td").text(UFOsighting.comments);
+});
 
 
